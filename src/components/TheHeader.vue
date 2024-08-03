aa8e27d349cc74d3a4471ddaf43fbe98554032cf
Obliviate <vyacheslavovna.an@mail.ru>
2024-08-03 17:25:29 +0500

    feat: project structure

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,30 @@
+<template>
+  <header class="header">
+    <div class="header__logo">
+      <img src="../assets/images/logo.png" alt="Rick and Morty" />
+    </div>
+    <nav class="header__nav">
+      <ul>
+        <li>
+          <routerLink to="Home" class="header_link">Главная</routerLink>
+        </li>
+        <li>
+          <routerLink to="Episodes" class="header_link">Эпизоды</routerLink>
+        </li>
+        <li>
+          <routerLink to="Locations" class="header_link">Локации</routerLink>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+.header {
+  &__logo {
+    width: 80vw;
+  }
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,32 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Home from '../views/Home.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'Home',
+      component: Home
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/Episodes',
+      name: 'Episodes',
+      component: () => import('../views/Episodes/index.vue')
+    },
+    {
+      path: '/Locations',
+      name: 'Locations',
+      component: () => import('../views/Locations/index.vue')
     }
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (About.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import('../views/AboutView.vue')
+    // }
   ]
 })
 
