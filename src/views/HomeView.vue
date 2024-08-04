7adaaca1ee816905da6995323cf0f80373e924db
Obliviate <vyacheslavovna.an@mail.ru>
2024-08-05 00:06:31 +0500

    feat: structure

---
+++ src/views/HomeView.vue
@@ -0,0 +1,27 @@
+<template>
+  <section class="home-view container">
+    <ul>
+      <li>
+        <routerLink>
+          <article>
+            <div>
+              <img src="" alt="" />
+            </div>
+            <h3></h3>
+          </article>
+        </routerLink>
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script setup>
+import { onBeforeMount, ref } from 'vue'
+
+const characters = null
+const loading = ref(true)
+
+onBeforeMount(() => {})
+</script>
+
+<style lang="scss" scoped></style>
