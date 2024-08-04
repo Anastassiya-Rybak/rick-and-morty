5013e47b3c1b8473e8dced896db759a9ebacde6e
Obliviate <vyacheslavovna.an@mail.ru>
2024-08-05 02:51:48 +0500

    fix: Api request

--- src/composables/useApiRequest.js
+++ src/composables/useApiRequest.js
@@ -3,9 +3,11 @@ export const useApiRequest = async (target) => {
 
   try {
     response = await fetch(`https://rickandmortyapi.com/api/${target}`)
+    if (response.ok) {
+      return await response.json()
+    }
   } catch (error) {
     console.error('Ошибка получения:', error)
     return { response: null, error: error.message }
   }
-  return response
 }
