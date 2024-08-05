e766ecb3a669f2e553caf1f2e4b696376150aec8
Obliviate <vyacheslavovna.an@mail.ru>
2024-08-05 17:39:46 +0500

    feat: character view info section

---
+++ src/views/CharacterView.vue
@@ -0,0 +1,127 @@
+<template>
+  <div class="character-page container">
+    <TheLoading v-if="!characterData" />
+    <article v-else class="character-page__info characters-info">
+      <figure class="characters-info__image">
+        <img :src="characterData.image" :alt="characterData.name" />
+      </figure>
+      <header class="characters-info__header">
+        <h2>{{ characterData.name }}</h2>
+      </header>
+      <section class="characters-info__content">
+        <ul>
+          <li><b>Статус:</b> {{ characterData.status }}</li>
+          <li><b>Вид:</b> {{ characterData.species }}</li>
+          <li><b>Пол:</b> {{ characterData.gender }}</li>
+          <li>
+            <b>Месторождение: </b>
+            <router-link :to="characterData.origin.url" style="color: #679c38; text-align: right">
+              {{ characterData.origin.name }}
+            </router-link>
+          </li>
+          <li>
+            <b>Местоположение: </b>
+            <router-link :to="characterData.location.url" style="color: #679c38; text-align: right">
+              {{ characterData.location.name }}
+            </router-link>
+          </li>
+        </ul>
+      </section>
+    </article>
+    <section class="character-page__episode-links"></section>
+  </div>
+</template>
+
+<script setup>
+import { useCharacterStore } from '@/stores/character'
+import { useApiRequest } from '@/composables/useApiRequest'
+import TheLoading from '@/components/TheLoading.vue'
+import { onBeforeUnmount, onMounted, ref } from 'vue'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+const characterStore = useCharacterStore()
+const error = ref(null)
+
+const characterData = ref(null)
+
+const fetchCharacter = async () => {
+  const data = await useApiRequest(`character/${route.params.id}`)
+  if (data.error) {
+    error.value = data.error
+  } else {
+    characterData.value = data
+  }
+}
+
+const findData = () => {
+  if (sessionStorage.getItem(route.fullPath)) {
+    characterData.value = JSON.parse(sessionStorage.getItem(route.fullPath))
+  } else if (characterStore.data) {
+    characterData.value = characterStore.data
+  } else {
+    fetchCharacter()
+  }
+}
+
+onMounted(() => {
+  findData()
+})
+
+onBeforeUnmount(() => {
+  characterStore.saveData(route.fullPath)
+})
+</script>
+
+<style lang="scss" scoped>
+@import './../assets/vars';
+
+.character-page {
+  background-color: $main-light;
+  border-radius: 1%;
+  overflow: hidden;
+  padding: 7vw;
+  margin-top: 5vh;
+  box-shadow: 0 0.5vh 0.5vh 1px rgba(0, 0, 0, 0.089);
+}
+
+.characters-info {
+  &__image {
+    position: relative;
+    box-shadow: 0 0 0.15em 0 rgba(0, 0, 0, 0.466);
+
+    &::after {
+      content: '';
+      position: absolute;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      box-shadow: inset 0 0 0.8em 0 rgb(248, 248, 248) !important;
+    }
+
+    img {
+      object-fit: cover;
+    }
+  }
+
+  &__header h2 {
+    @include accentText($size: 1.5em);
+    color: rgba(0, 0, 0, 0.404);
+    text-align: center;
+    margin: 2vh auto;
+  }
+
+  &__content ul {
+    * {
+      text-align-last: justify;
+      color: black;
+      line-height: 1.5em;
+      font-size: 1em;
+    }
+
+    li {
+      border-bottom: 0.5px solid black;
+    }
+  }
+}
+</style>
